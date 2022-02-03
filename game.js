457d9b6642deac25c6f6fda62db4820c44a25122
simonbr2 <brandon.simon2@gmail.com>
2022-02-02 21:57:44 -0500

    added working buttons and a banner to display messages

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
-let userScore = 0;
+let playerScore = 0;
 let computerScore = 0;
-const button = document.querySelectorAll('.button')
-const playerSelection = playerSelectionPrompt;
-const computerSelection = computerPlay();
+let playerSelection;
+let computerSelection;
+const banner = document.querySelector("#banner");
+banner.textContent = "Good Luck!!"
+
+const buttons = document.querySelectorAll('.button')
 
 function computerPlay(){
   let options = ['rock', 'paper', 'scissors'];
@@ -17,32 +20,65 @@ function playerSelectionPrompt(){
 }
 
 function playRound(playerSelection, computerSelection){
-  console.log(`Player Selection is ${playerSelection}`)
-  console.log(`Computer Selection is ${computerSelection}`)
+  console.log(`Player Selection is ${playerSelection}`);
+  console.log(`Computer Selection is ${computerSelection}`);
   if (playerSelection === computerSelection){
-    console.log(`The computer chose ${computerSelection}` + " it's a tie!")
-    console.log(`The Score is You:${userScore} and Computer: ${computerScore}`)
+    banner.textContent = `The computer chose ${computerSelection}... it's a tie!`;
+    console.log(`The computer chose ${computerSelection}` + " it's a tie!");
+    console.log(`The Score is You:${playerScore} and Computer: ${computerScore}`);
   }
   else if (playerSelection == 'scissors' && computerSelection == "paper"
   || playerSelection == 'paper' && computerSelection == 'rock'
   || playerSelection == 'rock' && computerSelection == 'scissors'){
-    console.log(`The computer chose ${computerSelection}` + " you win!")
-    userScore++
-    console.log(`The Score is You:${userScore} and Computer: ${computerScore}`)
+    banner.textContent = `The computer chose ${computerSelection}... you win!`;
+    console.log(`The computer chose ${computerSelection}` + " you win!");
+    playerScore++;
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${computerScore}`;
+    console.log(`The Score is You:${playerScore} and Computer: ${computerScore}`)
   }
   else if (computerSelection == 'scissors' && playerSelection == "paper"
   || computerSelection == 'paper' && playerSelection == 'rock'
   || computerSelection == 'rock' && playerSelection == 'scissors'){
-    console.log(`The computer chose ${computerSelection}` + " you lost!")
-    computerScore++
-    console.log(`The Score is You:${userScore} and Computer: ${computerScore}`)
+    banner.textContent = `The computer chose ${computerSelection}... you lost!`;
+    console.log(`The computer chose ${computerSelection}` + " you lost!");
+    computerScore++;
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${computerScore}`;
+    console.log(`The Score is You:${playerScore} and Computer: ${computerScore}`);
 }
 }
 
+buttons.forEach((button)=>{button.addEventListener('click',()=>{
+
+  playerSelection = button.id;
+  computerSelection = computerPlay();
+  game();
+  })
+
+})
+
+const player = document.querySelector("#playerScore");
+player.textContent = `Player Score: ${playerScore}`;
+
+const computer = document.querySelector("#computerScore");
+computer.textContent = `Computer Score: ${computerScore}`;
+
+
 function game(){
-  playRound(playerSelectionPrompt(), computerPlay())
-  playRound(playerSelectionPrompt(), computerPlay())
-  playRound(playerSelectionPrompt(), computerPlay())
-  playRound(playerSelectionPrompt(), computerPlay())
-  playRound(playerSelectionPrompt(), computerPlay())
+  playRound(playerSelection, computerSelection);
+  if (playerScore == 5){
+    playerScore = 0;
+    computerScore = 0;
+    banner.textContent = "You won the game! congrats!"
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${computerScore}`;
+  }
+  else if (computerScore == 5){
+    playerScore = 0;
+    computerScore = 0;
+    banner.textContent = "You lost the game! lame :("
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${computerScore}`;
+  }
 }
